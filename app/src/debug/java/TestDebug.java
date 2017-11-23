b909733cc419447448ee45f493fd1f9ea27dcc26
HuangMingFrancis <2967134464@qq.com>
2017-11-23 17:55:14 +0800

    学习使用gradle构建app项目

---
+++ app/src/debug/java/TestDebug.java
@@ -0,0 +1,12 @@
+import android.util.Log;
+
+/**
+ * Created by Francis.Huang on 2017/11/23.
+ */
+
+public class TestDebug {
+    private static final String TAG = "TestDebug";
+    public static void testDebug(){
+        Log.i(TAG, "testDebug: ");
+    }
+}
---
+++ app/src/jniDebug/java/TestJniDebug.java
@@ -0,0 +1,12 @@
+import android.util.Log;
+
+/**
+ * Created by Francis.Huang on 2017/11/23.
+ */
+
+public class TestJniDebug {
+    private static final String TAG = "TestJniDebug";
+    public static void testDebug(){
+        Log.i(TAG, "TestJniDebug: ");
+    }
+}
